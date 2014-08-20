06b8683b2ad06880f727633504fac30fa7c938fe
zhongdj <zhongdj@gmail.com>
2014-08-20 19:10:32 +0800

    add main classes

---
+++ src/main/scala/net/imadz/web/explorer/HttpErrorRecorder.scala
@@ -0,0 +1,11 @@
+package net.imadz.web.explorer
+
+import akka.actor.Actor
+import akka.actor.Actor.Receive
+
+/**
+ * Created by geek on 8/20/14.
+ */
+class HttpErrorRecorder extends Actor {
+  override def receive: Receive = ???
+}
---
+++ src/main/scala/net/imadz/web/explorer/Main.scala
@@ -0,0 +1,8 @@
+package net.imadz.web.explorer
+
+/**
+ * Created by geek on 8/20/14.
+ */
+object Main extends App {
+
+}
