d4417deaa9bf56f677ea9e3756bf779e2c0ecc04
zhongdj <zhongdj@gmail.com>
2014-08-21 13:38:09 +0800

    add lifecycle control

--- src/main/scala/net/imadz/web/explorer/HttpUrlGetter.scala
+++ src/main/scala/net/imadz/web/explorer/HttpUrlGetter.scala
@@ -1,6 +1,6 @@
 package net.imadz.web.explorer
 
-import akka.actor.{ActorRef, Props, Actor}
+import akka.actor.{Terminated, ActorRef, Props, Actor}
 import akka.actor.Actor.Receive
 import akka.pattern.pipe
 
@@ -18,11 +18,18 @@ class HttpUrlGetter(httpRequest: HttpRequest) extends Actor {
 
   override def receive: Receive = {
     case p@PageRequest(_, url, pre, _) =>
+
       import scala.concurrent.ExecutionContext.Implicits.global
       val headers: Map[String, String] = httpRequest.headers
       client.get(headers)(url) onComplete {
-        case Success(body) => context.actorOf(HttpLinkParser.props(body, p, context.parent))
-        case Failure(BadStatus(code)) => context.actorOf(HttpErrorRecorder.props(code, p))
+        case Success(body) =>
+          HttpUrlGetter.parserCount += 1
+          val child = context.actorOf(HttpLinkParser.props(body, p, context.parent), "HttpLinkParser-" + HttpUrlGetter.parserCount)
+          context.watch(child)
+        case Failure(BadStatus(code)) =>
+          HttpUrlGetter.reporterCount += 1
+          val child = context.actorOf(HttpErrorRecorder.props(code, p), "HttpErrorRecorder-" + HttpUrlGetter.reporterCount)
+          context.watch(child)
         case Failure(t) => self ! p
       }
 
@@ -30,15 +37,22 @@ class HttpUrlGetter(httpRequest: HttpRequest) extends Actor {
       import scala.concurrent.ExecutionContext.Implicits.global
       val headers: Map[String, String] = httpRequest.headers
       client.get(headers)(url) onComplete {
-        case Success(ignore) => None
-        case Failure(BadStatus(code)) => context.actorOf(HttpErrorRecorder.props(code, i))
+        case Success(ignore) =>
+          None
+        case Failure(BadStatus(code)) =>
+          val child = context.actorOf(HttpErrorRecorder.props(code, i))
+          context.watch(child)
         case Failure(t) => self ! i
       }
+    case Terminated(child) =>
+      context.stop(self)
   }
 }
 
 object HttpUrlGetter {
 
+  var reporterCount : Int = 0
+  var parserCount : Int = 0
   def props(httpRequest: HttpRequest) = Props(classOf[HttpUrlGetter], httpRequest)
 
 }
