5c06c19ce9b8c8960096a29429855863190ae848
zhongdj <zhongdj@gmail.com>
2014-08-27 13:01:20 +0800

    test nike

--- src/main/scala/net/imadz/web/explorer/Main.scala
+++ src/main/scala/net/imadz/web/explorer/Main.scala
@@ -10,34 +10,30 @@ class Main extends Actor with ActorLogging {
 
   //val initialUrl = "http://hybris51-prod.benefitdigital.com.cn/";
 
-  val initialUrl = "http://www.nike.com/cn/zh_cn/";
+  val initialUrl = "http://help-en-cn.nike.com"
   val initialName = ""
 
   def headers =
     Map[String, String](
-      "Accept" -> "image/webp,*/*;q=0.8",
-//      "Accept-Encoding" -> "gzip,deflate,sdch",
-//      "Accept-Language" -> "en-US,en;q=0.8,zh-CN;q=0.6",
-//      "Cache-Control" -> "no-cache",
-//      "Connection" -> "keep-alive",
-//      "Pragma" -> "no-cache",
-//      "Referer" -> "http://www.se566.com/html/se/79306.html",
-//      "Host" -> "hybris51-prod.benefitdigital.com.cn",
-//      "Authorization" -> "Basic YmVuZWZpdDpoeWJyaXM=",
-//      "Cookie" -> "symfony=apmod5halbtlj89pps8a3vi5t6; 38tERD42XE=%7B%22cart%22%3A%5B%5D%2C%22shoppingCartNumberOfItems%22%3A0%2C%22user%22%3Anull%2C%22shoppingCartId%22%3A%22anonymous%22%2C%22customerCountry%22%3A%22CN%22%7D",
+      "Accept" -> "text/html,application/xhtml+xml,text/xml,application/xml;q=0.9,image/webp,image/GIF,image/jpeg,text/plain, image/png, image/tiff, image/x-icon",
+      //      "Accept-Encoding" -> "gzip,deflate,sdch",
+      //      "Accept-Language" -> "en-US,en;q=0.8,zh-CN;q=0.6",
+      //      "Cache-Control" -> "no-cache",
+      //      "Connection" -> "keep-alive",
+      //      "Pragma" -> "no-cache",
+      //      "Referer" -> "http://www.se566.com/html/se/79306.html",
+      //      "Host" -> "hybris51-prod.benefitdigital.com.cn",
+      //      "Authorization" -> "Basic YmVuZWZpdDpoeWJyaXM=",
+      //      "Cookie" -> "symfony=apmod5halbtlj89pps8a3vi5t6; 38tERD42XE=%7B%22cart%22%3A%5B%5D%2C%22shoppingCartNumberOfItems%22%3A0%2C%22user%22%3Anull%2C%22shoppingCartId%22%3A%22anonymous%22%2C%22customerCountry%22%3A%22CN%22%7D",
       "User-Agent" -> "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.94 Safari/537.36"
     )
 
 
-  val excludes = Set[String](
-    "http://www.ping.com",
-    "http://help-zh-cn.swoosh.com",
-    "http://nikeplus.nike.com"
-  )
+  val excludes = Set[String]()
 
-  val domainConstraints = Set("benefitdigital.com", "nike.com")
+  val domainConstraints = Set("http://help-en-cn.nike.com", "http://help-zh-cn.nike.com", "http://help-ja-jp.nike.com")
 
-  val dispatcher = context.actorOf(HttpRequestDispatcher.props(headers, excludes, initialUrl, initialName,domainConstraints, 100), HttpRequestDispatcher.name)
+  val dispatcher = context.actorOf(HttpRequestDispatcher.props(headers, excludes, initialUrl, initialName, domainConstraints, 100), HttpRequestDispatcher.name)
   context.watch(dispatcher)
 
   val httpErrorRecorder = context.actorOf(HttpErrorRecorder.props(), HttpErrorRecorder.name)
@@ -47,7 +43,7 @@ class Main extends Actor with ActorLogging {
   //val imageDownloadLead = context.actorOf(Props(classOf[ImgDownloadLead]), ImgDownloadLead.name)
 
   override def receive: Receive = LoggingReceive {
-    case url: String => context.actorOf(HttpRequestDispatcher.props(headers, excludes, url, initialName, domainConstraints, 10))
+    case url: String => context.actorOf(HttpRequestDispatcher.props(headers, excludes, url, initialName, domainConstraints, 100))
     case Terminated(dispatcherActor) =>
       AsyncWebClient.shutdown
       context.children foreach context.stop
@@ -60,5 +56,5 @@ class Main extends Actor with ActorLogging {
 
 object Main {
   val name = "Main"
-  val path = "akka://" + name+ "/user/app/"
+  val path = "akka://" + name + "/user/app/"
 }
