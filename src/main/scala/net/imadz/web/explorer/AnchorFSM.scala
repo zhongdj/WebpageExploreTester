efbb101e589878e139a59c99c5b727656f270b3f
TracyLu <hailinglu@126.com>
2014-09-05 06:39:21 +0800

    bug fix

--- src/main/scala/net/imadz/web/explorer/AnchorFSM.scala
+++ src/main/scala/net/imadz/web/explorer/AnchorFSM.scala
@@ -8,31 +8,31 @@ import net.imadz.web.explorer.ParserLead.ParseRequest
  * Created by Scala on 14-9-4.
  */
 class AnchorFSM(p: PageRequest) extends Actor with FSM[State, Data] with ActorLogging {
-  startWith(WaitAnchorStart, NoLineData)
+  startWith(WaitAnchorStart, Empty)
   val parserLead: ActorSelection = context.actorSelection(ParserLead.path)
 
   assert(null != parserLead)
 
   when(WaitAnchorStart) {
-    case Event(NewLine(rawLine), NoLineData) => {
+    case Event(NewLine(rawLine), Empty) => {
       try {
         val newLine = rawLine.trim
         if (newLine.contains("<a")) {
           val result = newLine.split("</a>")
-          if (result.length == 1) goto(WaitAnchorEnd) using WaitAnchorEndData(lines = List(result(0)))
+          if (result.length == 1) goto(WaitAnchorEnd) using Buffer(lines = List(result(0)))
           else {
             (0 until (result.length - 1)) map { x =>
               val body = result(x) + "</a>"
               parserLead ! ParseRequest(body, p)
             }
             if (result(result.length - 1).contains("<a")) {
-              goto(WaitAnchorEnd) using WaitAnchorEndData(lines = List(result(result.length - 1)))
+              goto(WaitAnchorEnd) using Buffer(lines = List(result(result.length - 1)))
             } else {
-              stay using NoLineData
+              stay using Empty
             }
           }
         } else {
-          stay using NoLineData
+          stay using Empty
         }
       } catch {
         case e: Exception => log.error(e, e.getMessage)
@@ -46,29 +46,29 @@ class AnchorFSM(p: PageRequest) extends Actor with FSM[State, Data] with ActorLo
   }
 
   when(WaitAnchorEnd) {
-    case Event(NewLine(rawLine), WaitAnchorEndData(data)) => {
+    case Event(NewLine(rawLine), Buffer(data)) => {
       try {
         val newLine = rawLine.trim
         val result = newLine.split("</a>")
-        if  (result.length == 0) {
-          val last = data mkString "" + "</a>"
-          parserLead ! ParseRequest(last, p)
-          goto(WaitAnchorStart) using NoLineData
-        } else if (result.length == 1) {
-          stay using WaitAnchorEndData(lines = data ::: List(result(0)))
-        } else {
-          var last = data mkString "" + result(0) + "</a>"
-          parserLead ! ParseRequest(last, p)
+        if (result.length == 0) {
+          val last = data :+ "</a>"
 
-          (1 until (result.length - 1)) map { x =>
-            last = result(x) + "</a>"
-            parserLead ! ParseRequest(last, p)
+          parserLead ! ParseRequest(last mkString, p)
+          goto(WaitAnchorStart) using Empty
+        } else if (result.length == 1) {
+          stay using Buffer(lines = data :+ result(0))
+        } else {
+          val last = data :+ result(0) :+ "</a>"
+          parserLead ! ParseRequest(last mkString, p)
+
+          1 until (result.length - 1) map { x =>
+            parserLead ! ParseRequest(result(x) + "</a>", p)
           }
 
           if (result(result.length - 1).contains("<a")) {
-            stay using WaitAnchorEndData(lines = List(result(result.length - 1)))
+            stay using Buffer(lines = List(result(result.length - 1)))
           } else {
-            goto(WaitAnchorStart) using NoLineData
+            goto(WaitAnchorStart) using Empty
           }
         }
       } catch {
@@ -95,9 +95,9 @@ object AnchorFSM {
 
   sealed trait Data
 
-  case object NoLineData extends Data
+  case object Empty extends Data
 
-  case class WaitAnchorEndData(lines: List[String]) extends Data
+  case class Buffer(lines: List[String]) extends Data
 
   def props(p: PageRequest) = Props(classOf[AnchorFSM], p)
 }
