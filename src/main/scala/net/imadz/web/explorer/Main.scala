93b8ba990f36cb7d020c77258b78e7800574fb01
zhongdj <zhongdj@gmail.com>
2014-09-11 08:49:17 +0800

    add header

--- src/main/scala/net/imadz/web/explorer/Main.scala
+++ src/main/scala/net/imadz/web/explorer/Main.scala
@@ -1,3 +1,4 @@
+
 package net.imadz.web.explorer
 
 import akka.actor._
@@ -76,23 +77,22 @@ object StateUpdate {
 }
 
 case class TestRun(targetUrl: String, headerText: String, exclusions: String, inclusions: String, checkImage: Boolean, depth: Int) {
-  def exclusionList = exclusions.split("\n").map(_.trim).toSet
+  val seperator: String = "\n"
 
-  def inclusionList = inclusions.split("\n").map(_.trim).toSet
-  
-  def headers: Map[String, String] =  Map[String, String](
-    "Accept" -> "text/html,application/xhtml+xml,text/xml,application/xml;q=0.9,image/webp,image/GIF,image/jpeg,text/plain, image/png, image/tiff, image/x-icon",
-    //      "Accept-Encoding" -> "gzip,deflate,sdch",
-    //      "Accept-Language" -> "en-US,en;q=0.8,zh-CN;q=0.6",
-    //      "Cache-Control" -> "no-cache",
-    //      "Connection" -> "keep-alive",
-    //      "Pragma" -> "no-cache",
-    //      "Referer" -> "http://www.se566.com/html/se/79306.html",
-    //      "Host" -> "hybris51-prod.benefitdigital.com.cn",
-    //      "Authorization" -> "Basic YmVuZWZpdDpoeWJyaXM=",
-    //      "Cookie" -> "symfony=apmod5halbtlj89pps8a3vi5t6; 38tERD42XE=%7B%22cart%22%3A%5B%5D%2C%22shoppingCartNumberOfItems%22%3A0%2C%22user%22%3Anull%2C%22shoppingCartId%22%3A%22anonymous%22%2C%22customerCountry%22%3A%22CN%22%7D",
-    "User-Agent" -> "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.94 Safari/537.36"
-  )
+  def exclusionList = exclusions.split(seperator).map(_.trim).toSet
+
+  def inclusionList = inclusions.split(seperator).map(_.trim).toSet
+
+  def headers: Map[String, String] = {
+    val result: List[(String, String)] = headerText.split(seperator).toList.map { line: String =>
+      val separator = line.indexOf(":")
+      (line.substring(0, separator).trim, line.substring(separator + 1).trim)
+    }
+
+    result.toMap ++ Map[String, String](
+      "Accept" -> "text/html,application/xhtml+xml,text/xml,application/xml;q=0.9,image/webp,image/GIF,image/jpeg,text/plain, image/png, image/tiff, image/x-icon"
+    )
+  }
 }
 
 object Main {
